16fb17e7454c8345f03d8a77548a582f4f95fe6f
donlaya1 <68100321@kmitl.ac.th>
2025-09-24 16:01:15 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied"
-  exit
+if [ "$#" -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    count=0
+    for arg in "$@"; do
+        echo "$arg"
+        count=$((count + 1))
+        [ $count -eq 3 ] && break
+    done
 fi
-
-for arg in "$@"; do
-  echo "$arg"
-done | head -n 3
